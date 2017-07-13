314abc9465a518460817eaf37496b86d9f959880
cloud-elric <humberto@2gom.com.mx>
2017-07-13 14:19:14 -0500

    Inicio de proyecto

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Agregar usuario</title>
+        <?php
+        include("scripts.php");
+        ?>
+    </head>
+
+    <body>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-md-offset-6">
+                    
+                </div>
+            </div>
+        </div>
+
+    </body>
+
+</html>
