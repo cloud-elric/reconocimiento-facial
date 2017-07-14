05462163fd894a4184e3dbd965c29d5fd713950c
cloud-elric <humberto@2gom.com.mx>
2017-07-14 12:58:52 -0500

    Se agregaron mensajes de guardado

--- cargar-foto.php
+++ cargar-foto.php
@@ -10,57 +10,118 @@
 
     <body>
 
-        <div class="container">
-            <div class="row">
-                <div class="col-md-10 col-md-offset-1">
-                    <br>
-                    <br>
-                    <br>
+        <?php 
+        include('templates/header.php');
+?>
 
-                   <div class="row"> 
-                        <div class="col-md-12 text-center">
-                            <button class="btn btn-primary" id="take">
-                                Tomar foto
-                            </button>
-                        </div>
-                    </div>
 
-                    <br>
 
-                    <div class="row">
-                        <div class="col-md-6">
-                            <video style="width:100%;" class="embed-responsive-item" id="v"></video>
+        <br><br><br>
+        <div class="container-fluid">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                    <div class="panel panel-default">
+                        <div class="panel-heading" role="tab" id="headingOne">
+                            <h4 class="panel-title">
+                                <a id="opcion-tomar-foto" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                    Tomar foto
+                                </a>
+                            </h4>
                         </div>
 
-                        <div class="col-md-6">
-                            <img style="width:100%;"  id="photo" alt="photo">    
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="input-group">
-                                <input type="text" class="form-control" id="nombre" name="nombre" placeholder="Nombre completo">
+                        <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+                            <div class="panel-body">
+                                
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label for="nombre">Ingrese su nombre completo:</label>
+                                            <input type="text" class="form-control" id="nombre" name="nombre" placeholder="Nombre completo">
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <br>
+
+                                <div class="row">
+                                    <div class="col-md-12 text-center">
+                                        <button class="btn btn-primary btn-block" id="take">
+                                            Tomar foto
+                                        </button>
+                                    </div>
+                                </div>
+
+                                <br>
+
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <video style="width:100%;" class="embed-responsive-item" id="v"></video>
+                                    </div>
+
+                                    <div class="col-md-6">
+                                        <img style="width:100%;"  id="photo" alt="photo">    
+                                    </div>
+                                </div>
+
+                                <br>
+                                <br>
+                                <div class="row">
+                                    <div class="col-md-12 text-center">
+                                        <button data-style="zoom-in" id="btn-guardar" class="btn btn-success btn-block ladda-button">
+                                            <span class="ladda-label">    
+                                                Guardar foto
+                                            </span>    
+                                        </button>
+                                    </div>
+                                </div>
+                            
+                                <canvas id="canvas" style="display:none;"></canvas>
                             </div>
                         </div>
                     </div>
-                    <br>
-                    <br>
-                    <div class="row">
-                        <div class="col-md-12 text-center">
-                            <button id="btn-guardar" class="btn btn-success">
-                                Guardar usuario
-                            </button>
+
+                    <div class="panel panel-default">
+                        <div class="panel-heading" role="tab" id="headingTwo">
+                            <h4 class="panel-title">
+                                <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                Subir foto
+                                </a>
+                            </h4>
+                        </div>
+                        <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+                            <div class="panel-body">
+                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                            </div>
                         </div>
                     </div>
-                    <canvas id="canvas" style="display:none;"></canvas>
-                   
-                    <script>
+                    
+                </div>
+            </div>
+        </div>
 
+    
+                    <script>
+                    var timesButton = 0;
                     $(document).ready(function(){
                         $("#btn-guardar").on("click", function(){
                             var canvas = document.getElementById('canvas');
                             var dataURL = canvas.toDataURL();
                             var nombre = $("#nombre").val();
+
+                            if( $('#nombre').val().length === 0 ) {
+                                swal("Espera", "Debes ingresar tu nombre", "warning");
+                                return false;
+                            }
+
+                            if(timesButton<1){
+                                swal("Espera", "Debes tomarte una foto", "warning");
+                                return false;
+                            }
+
+                            
+                            var l = Ladda.create(this);
+	 	                    l.start();
+                            
                             $.ajax({
                                 type: "POST",
                                 url: "vendor/guardar-imagen.php",
@@ -70,6 +131,12 @@
                                 },
                                 
                                 }).done(function(o) {
+                                timesButton = 0;
+                                $("#nombre").val('');
+                                $("#photo").attr('src', '');
+                                swal("Ok", "Imagen guardada", "success");
+                                $("#opcion-tomar-foto").trigger('click');
+                                l.stop();
                                 console.log('saved'); 
                                 // If you want the file to be visible in the browser 
                                 // - please modify the callback in javascript. All you
@@ -118,6 +185,12 @@
 
                                 // Listen for user click on the "take a photo" button
                                 document.getElementById('take').addEventListener('click', function(){
+
+                                    if( $('#nombre').val().length === 0 ) {
+                                         swal("Espera", "Debes ingresar tu nombre", "warning");
+                                        return false;
+                                    }
+
                                     if (videoPlaying){
                                         var canvas = document.getElementById('canvas');
                                         canvas.width = video.videoWidth;
@@ -125,6 +198,7 @@
                                         canvas.getContext('2d').drawImage(video, 0, 0);
                                         var data = canvas.toDataURL('image/webp');
                                         document.getElementById('photo').setAttribute('src', data);
+                                        timesButton++;
                                     }
                                 }, false);
 
@@ -135,10 +209,6 @@
                             }
                         })();
                     </script>
-
-                </div>
-            </div>
-        </div>
 
     </body>
 
--- scripts.php
+++ scripts.php
@@ -1,5 +1,16 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="http://t4t5.github.io/sweetalert/dist/sweetalert.css">
+<link rel="stylesheet" href="https://msurguy.github.io/ladda-bootstrap/dist/ladda-themeless.min.css">
 <link rel="stylesheet" href="css/site.css">
+
 <script
   src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+  
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script
+  src="http://t4t5.github.io/sweetalert/dist/sweetalert-dev.js"></script>
+<script
+  src="https://msurguy.github.io/ladda-bootstrap/dist/spin.min.js"></script>   
+<script
+  src="https://msurguy.github.io/ladda-bootstrap/dist/ladda.min.js"></script> 
+
