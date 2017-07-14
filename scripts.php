c7992c2e386793400b4cca73ea6c1b68f51ef411
cloud-elric <humberto@2gom.com.mx>
2017-07-14 11:14:52 -0500

    Se agrego pantalla inicial

--- scripts.php
+++ scripts.php
@@ -1,4 +1,5 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="css/site.css">
 <script
   src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
