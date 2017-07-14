26bb7b77de3720b1d8c1963f8eec5f0540477b0f
cloud-elric <humberto@2gom.com.mx>
2017-07-14 14:04:31 -0500

    Habilidad de subir una imagen

--- cargar-foto.php
+++ cargar-foto.php
@@ -83,14 +83,46 @@
                     <div class="panel panel-default">
                         <div class="panel-heading" role="tab" id="headingTwo">
                             <h4 class="panel-title">
-                                <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                <a id="opcion-subir-foto" class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
                                 Subir foto
                                 </a>
                             </h4>
                         </div>
                         <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
                             <div class="panel-body">
-                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label for="nombre">Ingrese su nombre completo:</label>
+                                            <input type="text" class="form-control" id="nombre2" name="nombre" placeholder="Nombre completo">
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <br>
+
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <input type="file" id="input-subir-imagen"/>
+                                    </div>
+
+                                    <div class="col-md-6">
+                                        <img style="width:100%;"  id="photo2" alt="photo">    
+                                    </div>
+                                </div>
+
+                                <br>
+                                <br>
+                                <div class="row">
+                                    <div class="col-md-12 text-center">
+                                        <button data-style="zoom-in" id="btn-guardar2" class="btn btn-success btn-block ladda-button">
+                                            <span class="ladda-label">    
+                                                Guardar foto
+                                            </span>    
+                                        </button>
+                                    </div>
+                                </div>
+                            
                             </div>
                         </div>
                     </div>
@@ -100,9 +132,56 @@
         </div>
 
     
-                    <script>
+        <script>
+            function agregarPreviewImage(element, jqelement){
+	            var file = element.files[0];
+
+                if (!file) {
+
+                    return false;
+                }
+
+                var imagefile = file.type;
+
+                var filename = jqelement.val();
+
+                if (filename.substring(3, 11) == 'fakepath') {
+                    filename = filename.substring(12);
+                }// remove c:\fake at beginning from localhost chrome
+                // var url = base+'usrUsuarios/guardarFotosCompetencia';
+
+                var match = [ "image/jpeg", "image/jpg", 'image/png' ];
+
+                if (!((imagefile == match[0]) || (imagefile == match[1]) || (imagefile == match[2]))) {
+
+                    swal("Espera", "Archivo no aceptado por el sistema", "warning");
+
+                    return false;
+                }
+
+                if (element.files && element.files[0]) {
+                    var reader = new FileReader();
+
+                    reader.onload = function(e) {
+                        var token = jqelement.data('token');
+                        $('#photo2').attr('src',e.target.result);
+                        
+                        //$('#modal-sustain-art-' + token+' .sustain-art-cont-images-dialog-img').css(
+                        //        'background-image', 'url(' + e.target.result + ')');
+                        
+
+                    }
+
+                    reader.readAsDataURL(element.files[0]);
+                }
+            }
                     var timesButton = 0;
                     $(document).ready(function(){
+
+                        $("#input-subir-imagen").on("change", function(){
+                            agregarPreviewImage(this, $(this));
+                        });
+
                         $("#btn-guardar").on("click", function(){
                             var canvas = document.getElementById('canvas');
                             var dataURL = canvas.toDataURL();
@@ -136,6 +215,50 @@
                                 $("#photo").attr('src', '');
                                 swal("Ok", "Imagen guardada", "success");
                                 $("#opcion-tomar-foto").trigger('click');
+                                l.stop();
+                                console.log('saved'); 
+                                // If you want the file to be visible in the browser 
+                                // - please modify the callback in javascript. All you
+                                // need is to return the url to the file, you just saved 
+                                // and than put the image in your browser.
+                                });
+                        });
+                    });
+
+                     $("#btn-guardar2").on("click", function(){
+                            var canvas = $("#photo2");
+                            var dataURL = canvas.attr('src');
+                            var nombre = $("#nombre2").val();
+
+                            if( $('#nombre2').val().length === 0 ) {
+                                swal("Espera", "Debes ingresar tu nombre", "warning");
+                                return false;
+                            }
+
+                            if( document.getElementById("photo2").files.length == 0 ){
+                                swal("Espera", "Debes subir una foto", "warning");
+                                return false;
+                            }
+
+                            
+                            var l = Ladda.create(this);
+	 	                    l.start();
+                            
+                            $.ajax({
+                                type: "POST",
+                                url: "vendor/guardar-imagen.php",
+                                data: { 
+                                    imgBase64: dataURL,
+                                    nombre: nombre
+                                },
+                                
+                                }).done(function(o) {
+                                timesButton = 0;
+                                $("#nombre2").val('');
+                                $("#photo2").attr('src', '');
+                                swal("Ok", "Imagen guardada", "success");
+                                $("#opcion-subir-foto").trigger('click');
+                                $("#input-subir-imagen").val('');
                                 l.stop();
                                 console.log('saved'); 
                                 // If you want the file to be visible in the browser 
