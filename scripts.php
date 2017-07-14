0b262d16afbcb54073ff96393de79fb80bc1ec1e
cloud-elric <humberto@2gom.com.mx>
2017-07-14 13:09:22 -0500

    Actualizacion de scripts

--- scripts.php
+++ scripts.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<link rel="stylesheet" href="http://t4t5.github.io/sweetalert/dist/sweetalert.css">
+<link rel="stylesheet" href="css/sweetalert.css">
 <link rel="stylesheet" href="css/ladda.css">
 <link rel="stylesheet" href="css/site.css">
 
@@ -8,7 +8,7 @@
   
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <script
-  src="http://t4t5.github.io/sweetalert/dist/sweetalert-dev.js"></script>
+  src="js/sweetalert.js"></script>
 <script
   src="js/spin.js"></script>   
 <script
